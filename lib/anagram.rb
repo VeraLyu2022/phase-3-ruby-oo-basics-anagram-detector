04532cad9864621a8d72fa08b96157526c77057a
Vera Lyu <465784942@qq.com>
2023-04-30 12:11:37 +1000

    it is very hard one

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+     @word = word
+    end
+
+    
+
+   
+
+    def match(words)
+        
+        new_anagram = words.select {|word| anagram?(word, @word)}
+
+    end
+    def anagram?(word1, word2)
+        sort_chars(word1) == sort_chars(word2)
+    end
+    def sort_chars(word)
+        word.downcase.chars.sort.join
+    end
+
+
+end
